3324288700cdf596b563b7c19cdd402f88391688
Nanaka Yoshimura <7ka.yoshimura@gmail.com>
2020-07-11 13:23:56 +0900

    狂人、占い師作成

---
+++ src/components/Roles/FortuneTeller.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: "fortuneteller",
+  data() {
+    return {
+      title: "占い師"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Roles/Madman.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: "madman",
+  data() {
+    return {
+      title: "狂人"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
