4bf1e848426dae8a365e11b4f9765f9f79e2a41b
Tatsuya <tatsuyayoshiyasu@gmail.com>
2020-07-14 23:14:40 +0900

    Adding Werewolf and Knight role files

---
+++ src/components/Roles/Knight.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: "knight",
+    data() {
+        return {
+            title: "騎士"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Roles/Werewolf.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: "werewolf",
+    data() {
+        return {
+            title: "人狼"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
