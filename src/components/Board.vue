c44a4319e51bd02978b3bb354027edde0d12e78c
Nanaka Yoshimura <7ka.yoshimura@gmail.com>
2020-08-01 13:55:22 +0900

    ADD check method

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,34 +1,39 @@
 <template>
-    <div class="board">
-        <Player v-for="player in players" :key="player.id" 
-                :id="player.id" :name="player.name" :role="player.role" 
-                :status="player.status" :otherPlayers="players"
-                :gameState="gameState"
-                @attack="attack">
-        </Player>
-    </div>
+  <div class="board">
+    <Player
+      v-for="player in players"
+      :key="player.id"
+      :id="player.id"
+      :name="player.name"
+      :role="player.role"
+      :status="player.status"
+      :otherPlayers="players"
+      :gameState="gameState"
+      @attack="attack"
+      @check="check"
+    ></Player>
+  </div>
 </template>
 
 <script>
 import Player from "./Player";
 
 export default {
-    name: "Players",
-    components: {
-        Player
+  name: "Players",
+  components: {
+    Player,
+  },
+  props: {
+    players: Array,
+    gameState: String,
+  },
+  methods: {
+    attack(id) {
+      this.$emit("attack", id);
     },
-    props:{
-        players: Array,
-        gameState: String,
-    },
-    methods:{
-        attack(id) {
-            this.$emit("attack", id)
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
