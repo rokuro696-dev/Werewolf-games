a915892ed5f64673f851aa61845aa59621c49529
Nanaka Yoshimura <7ka.yoshimura@gmail.com>
2020-08-01 13:50:26 +0900

    ADD FortuneTeller Action

--- src/components/Roles/FortuneTeller.vue
+++ src/components/Roles/FortuneTeller.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>{{ title }}</div>
+  <div>
+    {{ title }}
+    <br />
+    <br />占う
+    <li v-for="target in validTargets" :key="target.id">
+      <input type="submit" :value="target.name" class="btn" @click="check(target.id)" />
+    </li>
+  </div>
 </template>
 
 <script>
@@ -7,9 +14,18 @@ export default {
   name: "fortuneteller",
   data() {
     return {
-      title: "占い師"
+      title: "占い師",
     };
-  }
+  },
+  props: {
+    validTargets: Array,
+  },
+  methods: {
+    check(id) {
+      alert("clicked on " + id + " from fortuneteller.vue");
+      this.$emit("check", id);
+    },
+  },
 };
 </script>
 
