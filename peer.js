0b07458036787b13dd375c602dc93c4df25022c5
kish-k <xnd29487@gmail.com>
2020-10-31 15:43:57 +0900

    refactor: #3 refactor codes

--- peer.js
+++ peer.js
@@ -1,12 +1,62 @@
 module.exports = class Peer {
-  constructor(socket_id, name) {
-      this.id = socket_id
-      this.name = name
-      this.transports = new Map()
-      this.consumers = new Map()
-      this.producers = new Map()
+  constructor(socketId, name) {
+    this.id = socketId
+    this.name = name
+    this.transports = new Map()
+    this.consumers = new Map()
+    this.producers = new Map()
   }
 
-  // getter と setterの追加
+  // Peer配下のCRUDは基本こちらで定義する
 
+  setTransport(id, transport) {
+    this.transports.set(id, transport);
+  }
+
+  getTransport(id) {
+    return this.transports.get(id);
+  }
+
+  setProducer(id, producer) {
+    this.producers.set(id, producer);
+  }
+
+  setConsumer(id, consumer) {
+    this.consumer.set(id, consumer);
+  }
+
+
+  async createConsumer(consumerTransportId, producerId, rtpCapabilities) {
+
+    let consumerTransport = this.transports.get(consumerTransportId)
+    let consumer = null
+
+    try {
+      consumer = await consumerTransport.consume({
+        producerId: producerId,
+        rtpCapabilities,
+        paused: false
+        // paused: producer.kind === 'video',
+      });
+      console.log(`create new consumer for producer: ${producerId} consumerId: ${consumer.id} client Id: ${this.id}`);
+
+      this.consumers.set(consumer.id, consumer)
+    } catch (error) {
+      console.error('consume failed', error);
+      return;
+    }
+
+    if (consumer.type === 'simulcast') {
+      await consumer.setPreferredLayers({ spatialLayer: 2, temporalLayer: 2 });
+    }
+
+    return {
+      producerId: producerId,
+      id: consumer.id,
+      kind: consumer.kind,
+      rtpParameters: consumer.rtpParameters,
+      type: consumer.type,
+      producerPaused: consumer.producerPaused
+    }
+  }
 }
--- rtc-client.js
+++ rtc-client.js
@@ -13,7 +13,6 @@ const mediaType = {
 
 let device;
 let socket;
-let ownProducerId;
 
 let producers = new Map();
 let consumers = new Map();
@@ -23,6 +22,9 @@ let consumerTransport;
 
 let consumeVideoDoms = {};
 
+const user = 'test_user'; //get user name from dom
+const roomName = 'test_room';
+
 
 if (typeof navigator.mediaDevices.getDisplayMedia === 'undefined') {
   //TODO: media のサポートが内場合のエラー出力処理
@@ -31,13 +33,13 @@ if (typeof navigator.mediaDevices.getDisplayMedia === 'undefined') {
 // web loaded
 window.addEventListener("load", function () {
   console.log("Page load, connect WebSocket");
-  try{
+  try {
     connect();
 
-  } catch (err){
+  } catch (err) {
     console.error(err);
   }
-  
+
   if ("adapter" in window) {
     console.log(
       // eslint-disable-next-line no-undef
@@ -48,12 +50,8 @@ window.addEventListener("load", function () {
   }
 });
 
-// assign DOMS when enable audio
-// await publish(mediatype.audio);  
-let user = 'test_user'; //get user name from dom
 
-
-async function connect(){
+async function connect() {
   const opts = {
     path: '/server',
     transports: ['websocket'],
@@ -63,140 +61,153 @@ async function connect(){
   socket = socketClient(serverUrl, opts);
   socket.request = socketPromise(socket);
 
-  await joinRoom(user);
+  createRoom(roomName);
+  await joinRoom(user, roomName);
   initSocket();
 }
 
-async function joinRoom(user){
-  socket.request('join', {
-    user //change name user
-    // room_id
-  }).then(async function (e) {
-      console.log(e)
-      const data = await socket.request('getRouterRtpCapabilities');
-      await loadDevice(data.data)
-      await initTransports(device)
-      socket.emit('getProducers')
+async function createRoom(roomName){
+  console.log(`room name ; ${roomName}`)
+  socket.request('createRoom', {
+    roomName: roomName,
+  }).then(async function(e){
+    //check response
+    console.log(e)
+  })
+}
 
-      publish(mediaType.video);
+async function joinRoom(user, roomName) {
+  socket.request('join', {
+    user: user, //TODO change name user
+    roomName: roomName
+  }).then(async function (e) {
+    console.log(e)
+    const rtpCapabilities = await socket.request('getRouterRtpCapabilities');
+    await loadDevice(rtpCapabilities)
+    await initTransports(device)
+    socket.emit('getProducers')
+
+    // TODO assign DOMS when enable audio
+    // await publish(mediatype.audio);  
+    publish(mediaType.video);
   }).catch(e => {
-      console.log(e)
+    console.log(e)
   })
 
 }
 
 
-async function initTransports(device){
+async function initTransports(device) {
   // init producerTransport
   {
     const data = await socket.request('createWebRtcTransport', {
-        forceTcp: false,
-        rtpCapabilities: device.rtpCapabilities,
+      forceTcp: false,
+      rtpCapabilities: device.rtpCapabilities,
     })
     if (data.error) {
-        console.error(data.error);
-        return;
+      console.error(data.error);
+      return;
     }
 
     producerTransport = device.createSendTransport(data);
 
     producerTransport.on('connect', async function ({
-        dtlsParameters
+      dtlsParameters
     }, callback, errback) {
-        socket.request('connectTransport', {
-                dtlsParameters,
-                transport_id: data.id
-            })
-            .then(callback)
-            .catch(errback)
+      socket.request('connectTransport', {
+        dtlsParameters,
+        transport_id: data.id
+      })
+        .then(callback)
+        .catch(errback)
     });
 
     producerTransport.on('produce', async function ({
-        kind,
-        rtpParameters
+      kind,
+      rtpParameters
     }, callback, errback) {
-        try {
-            const {
-                producer_id
-            } = await socket.request('produce', {
-                producerTransportId: producerTransport.id,
-                kind,
-                rtpParameters,
-            });
-            callback({
-                id: producer_id
-            });
-        } catch (err) {
-            errback(err);
-        }
+      try {
+        const {
+          producer_id
+        } = await socket.request('produce', {
+          producerTransportId: producerTransport.id,
+          kind,
+          rtpParameters,
+        });
+        callback({
+          id: producer_id
+        });
+      } catch (err) {
+        errback(err);
+      }
     });
 
     producerTransport.on('connectionstatechange', function (state) {
-        switch (state) {
-            case 'connecting':
+      switch (state) {
+        case 'connecting':
 
-                break;
+          break;
 
-            case 'connected':
-                //localVideo.srcObject = stream
-                break;
+        case 'connected':
+          //localVideo.srcObject = stream
+          break;
 
-            case 'failed':
-                producerTransport.close();
-                break;
+        case 'failed':
+          producerTransport.close();
+          break;
 
-            default:
-                break;
-        }
+        default:
+          break;
+      }
     });
-}
+  }
 
-// init consumerTransport
-{
+  // init consumerTransport
+  {
     const data = await socket.request('createWebRtcTransport', {
-        forceTcp: false,
+      forceTcp: false,
     });
     if (data.error) {
-        console.error(data.error);
-        return;
+      console.error(data.error);
+      return;
     }
 
     // only one needed
     consumerTransport = device.createRecvTransport(data);
     consumerTransport.on('connect', function ({
-        dtlsParameters
+      dtlsParameters
     }, callback, errback) {
-        socket.request('connectTransport', {
-                transport_id: consumerTransport.id,
-                dtlsParameters
-            })
-            .then(callback)
-            .catch(errback);
+      socket.request('connectTransport', {
+        transport_id: consumerTransport.id,
+        dtlsParameters
+      })
+        .then(callback)
+        .catch(errback);
     });
 
     consumerTransport.on('connectionstatechange', async function (state) {
-        switch (state) {
-            case 'connecting':
-                break;
+      switch (state) {
+        case 'connecting':
+          break;
 
-            case 'connected':
-                //remoteVideo.srcObject = await stream;
-                //await socket.request('resume');
-                break;
+        case 'connected':
+          //remoteVideo.srcObject = await stream;
+          //await socket.request('resume');
+          break;
 
-            case 'failed':
-                consumerTransport.close();
-                break;
+        case 'failed':
+          consumerTransport.close();
+          break;
 
-            default:
-                break;
-        }
+        default:
+          break;
+      }
     });
-}
+  }
 }
 
 async function initSocket() {
- 
+
   socket.on('disconnect', () => {
     //接続切断処理 エラー出力を画面側でするとよいかも
   });
@@ -208,7 +219,7 @@ async function initSocket() {
   socket.on('newProducers', (data) => {
     // console.log('new producer data ' + data)
 
-    for (let {producer_id} of data) {
+    for (let { producer_id } of data) {
       console.log('new producer data ' + producer_id)
       subscribe(producer_id)
     }
@@ -234,45 +245,45 @@ async function publish(type) {
 
   let audio = false
   switch (type) {
-      case mediaType.audio:
-          mediaConstraints = {
-              // audio: {
-              //     deviceId: deviceId
-              // },
-              audio: true,
-              video: false
-          }
-          audio = true
-          break
-      case mediaType.video:
-          mediaConstraints = {
-              audio: false,
-              video: true,
-              // video: {
-              //     width: {
-              //         min: 640,
-              //         ideal: 1920
-              //     },
-              //     height: {
-              //         min: 400,
-              //         ideal: 1080
-              //     },
-                  // deviceId: deviceId
-                  /*aspectRatio: {
-                      ideal: 1.7777777778
-                  }*/
-              // }
-          }
-          break
-      default:
-          return
+    case mediaType.audio:
+      mediaConstraints = {
+        // audio: {
+        //     deviceId: deviceId
+        // },
+        audio: true,
+        video: false
+      }
+      audio = true
+      break
+    case mediaType.video:
+      mediaConstraints = {
+        audio: false,
+        video: true,
+        // video: {
+        //     width: {
+        //         min: 640,
+        //         ideal: 1920
+        //     },
+        //     height: {
+        //         min: 400,
+        //         ideal: 1080
+        //     },
+        // deviceId: deviceId
+        /*aspectRatio: {
+            ideal: 1.7777777778
+        }*/
+        // }
+      }
+      break
+    default:
+      return
   }
 
   if (!device.canProduce('video') && !audio) {
-      console.error('cannot produce video');
-      return;
+    console.error('cannot produce video');
+    return;
   }
-    
+
   let stream;
 
   try {
@@ -281,42 +292,42 @@ async function publish(type) {
     const params = { track };
 
     if (!audio) {
-        params.encodings = [{
-                rid: 'r0',
-                maxBitrate: 100000,
-                //scaleResolutionDownBy: 10.0,
-                scalabilityMode: 'S1T3'
-            },
-            {
-                rid: 'r1',
-                maxBitrate: 300000,
-                scalabilityMode: 'S1T3'
-            },
-            {
-                rid: 'r2',
-                maxBitrate: 900000,
-                scalabilityMode: 'S1T3'
-            }
-        ];
-        params.codecOptions = {
-            videoGoogleStartBitrate: 1000
-        };
+      params.encodings = [{
+        rid: 'r0',
+        maxBitrate: 100000,
+        //scaleResolutionDownBy: 10.0,
+        scalabilityMode: 'S1T3'
+      },
+      {
+        rid: 'r1',
+        maxBitrate: 300000,
+        scalabilityMode: 'S1T3'
+      },
+      {
+        rid: 'r2',
+        maxBitrate: 900000,
+        scalabilityMode: 'S1T3'
+      }
+      ];
+      params.codecOptions = {
+        videoGoogleStartBitrate: 1000
+      };
     }
-  
+
     producer = await producerTransport.produce(params);
     producers.set(producer.id, producer)
 
 
     let elem
     if (!audio) {
-        elem = document.querySelector('#myVideo')
-        // elem = document.createElement('video')
-        elem.srcObject = stream
-        elem.id = producer.id
-        // elem.playsinline = false
-        // elem.autoplay = true
-        // elem.className = "vid"
-        // localMediaEl.appendChild(elem)
+      elem = document.querySelector('#myVideo')
+      // elem = document.createElement('video')
+      elem.srcObject = stream
+      elem.id = producer.id
+      // elem.playsinline = false
+      // elem.autoplay = true
+      // elem.className = "vid"
+      // localMediaEl.appendChild(elem)
     }
   } catch (err) {
     console.log('[ERROR] producer cannot create')
@@ -344,58 +355,58 @@ async function getUserMedia(transport, mediaConstraints) {
 
 
 
-
 async function subscribe(producer_id) {
 
   consume(producer_id).then(function ({
-      consumer,
-      stream,
-      kind
+    consumer,
+    stream,
+    kind
   }) {
 
-      // console.log(`received consumer info ${consumer.id}, kind ${kind}`)
+    // console.log(`received consumer info ${consumer.id}, kind ${kind}`)
 
-      consumers.set(consumer.id, consumer)
+    consumers.set(consumer.id, consumer)
 
-      let elem;
-      if (kind === 'video') {
-        // TODO: ここでDOMを追加していく処理をいれる
-        domsTag = '#myVideo' + (Object.keys(consumeVideoDoms).length + 2)
-        console.log(domsTag)
-        consumeVideoDoms[consumer.id] = domsTag;
+    let elem;
+    if (kind === 'video') {
+      // TODO: ここでDOMを追加していく処理をいれる
+      domsTag = '#myVideo' + (Object.keys(consumeVideoDoms).length + 2)
+      console.log(domsTag)
+      consumeVideoDoms[consumer.id] = domsTag;
 
-        elem = document.querySelector(domsTag)
+      elem = document.querySelector(domsTag)
 
-        elem.srcObject = stream
-        elem.id = consumer.id
-        // elem.playsinline = false
-        // elem.autoplay = true
-        // elem.className = "vid"
-      } else {
-          elem = document.createElement('audio')
-          elem.srcObject = stream
-          elem.id = consumer.id
-          elem.playsinline = false
-          elem.autoplay = true
-          remoteAudioEl.appendChild(elem)
-      }
+      elem.srcObject = stream
+      elem.id = consumer.id
+      // elem.playsinline = false
+      // elem.autoplay = true
+      // elem.className = "vid"
+    } else {
+      elem = document.createElement('audio')
+      elem.srcObject = stream
+      elem.id = consumer.id
+      elem.playsinline = false
+      elem.autoplay = true
+      remoteAudioEl.appendChild(elem)
+    }
 
-      consumer.on('trackended', function () {
-          removeConsumer(consumer.id)
-      })
-      consumer.on('transportclose', function () {
-          removeConsumer(consumer.id)
-      })
+    consumer.on('trackended', function () {
+      removeConsumer(consumer.id)
+    })
+    consumer.on('transportclose', function () {
+      removeConsumer(consumer.id)
+    })
   })
 }
 
 async function consume(reqeustProducerId) {
   // console.log('start request consume. producer id ' +reqeustProducerId);
   const { rtpCapabilities } = device;
-  const data = await socket.request('consume', { 
+  const data = await socket.request('consume', {
     consumerTransportId: consumerTransport.id,
     producerId: reqeustProducerId,
-    rtpCapabilities });
+    rtpCapabilities
+  });
   const {
     producerId,
     id,
